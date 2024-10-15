2c263819f92b6a114213d8879f6d6cb5800ba03f
sndrlzbth <sandra.lizbeth2908@gmail.com>
2024-10-15 13:31:14 -0700

    JWT

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,16 +5,26 @@ import { AuthService } from './auth.service';
 import { UsersModule } from '../users/users.module';
 import { AuthController } from './auth.controller';
 import { JwtStrategy } from './jwt.stratergy';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   providers: [AuthService, JwtStrategy],
   imports: [
     UsersModule,
     PassportModule,
-    JwtModule.register({
-      secret: 'DFGBLSOEMTVMSLWO', 
-      signOptions: { expiresIn: '1h' },
-    }),
+    JwtModule.registerAsync({
+      global: true,
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService)=>({
+        secret: configService.get<string>('JWT_SECRET'),
+      }),
+      inject: [ConfigService],
+    })
+    // JwtModule.register({
+    //   global: true,
+    //   secret: 'DFGBLSOEMTVMSLWO', 
+    //   signOptions: { expiresIn: '1h' },
+    // }),
   ],
   controllers: [AuthController],
   exports: [AuthService],
