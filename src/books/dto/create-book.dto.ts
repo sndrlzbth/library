4b6480ae6c465d6c30a30dfea65ed90c91d8e78c
sndrlzbth <sandra.lizbeth2908@gmail.com>
2024-10-15 13:40:02 -0700

    UPDT

--- src/books/dto/create-book.dto.ts
+++ src/books/dto/create-book.dto.ts
@@ -1,5 +1,11 @@
+import { ApiProperty } from "@nestjs/swagger"
 export class CreateBookDto {
+    @ApiProperty({ example: 'The Lord of the Rings', description: 'El título del libro' })
     title: string;
+
+    @ApiProperty({ example: 'J.R.R. Tolkien', description: 'El autor del libro' })
     author: string;
+
+    @ApiProperty({ example: '1954', description: 'El año del libro' })
     year: number;
 }
--- src/books/dto/update-book.dto.ts
+++ src/books/dto/update-book.dto.ts
@@ -1,5 +1,9 @@
+import { ApiProperty } from "@nestjs/swagger"
 export class UpdateBookDto {
+    @ApiProperty({ example: 'The Lord of the Rings', description: 'El título del libro' })
     title?: string;
+    @ApiProperty({ example: 'J.R.R. Tolkien', description: 'El autor del libro' })
     author?: string;
+    @ApiProperty({ example: '1954', description: 'El año del libro' })
     year?: number;
 }
